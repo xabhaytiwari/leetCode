a8b1111527bd4a522c4f0efb0841d7d9e6e54b2d
Abhay Tiwari <emailatabhay@gmail.com>
2025-04-09 19:46:15 +0530

    Solved Fibonnaci using Top-Down Memoization Approach

--- DynamicProgramming/fibonacciMemoization.cpp
+++ DynamicProgramming/fibonacciMemoization.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 int fib(int n, vector<int>& dp) {
     if(n <= 1) return n;
+    if(dp[n] != -1) return dp[n];
 
     dp[n] = fib(n - 1, dp) + fib(n - 2, dp);
     return dp[n];
