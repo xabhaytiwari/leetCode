0ce7eb67469520f1a7a4eaf0801733ae0c961b0f
Abhay Tiwari <emailatabhay@gmail.com>
2025-07-10 17:14:58 +0530

    Solved 26 on 10th July

---
+++ TwoPointers/344ReverseString.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class ReverseString {
+    public:
+        void reverseString(vector<char> &s) {
+            int left = 0, right = s.size() - 1;
+            while(left < right) { //Kyunki same ka reverse karne ka koi sense nahin banta
+                swap(s[left++], s[right--]);
+            }
+        }
+
+};
+
+int main() {
+    vector<char> s = {'h', 'e', 'l', 'l', 'o'};
+    ReverseString solution;
+    solution.reverseString(s);
+    for(const char ch : s) {
+        cout << ch;
+    }
+    return 0;
+}
