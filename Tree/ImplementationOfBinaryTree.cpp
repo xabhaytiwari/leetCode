d816f10132bb356c9249edb39255de6b10f32bff
Abhay Tiwari <emailatabhay@gmail.com>
2025-07-22 07:29:00 +0000

    Solved on 22nd July

--- Tree/ImplementationOfBinaryTree.cpp
+++ Tree/ImplementationOfBinaryTree.cpp
@@ -59,6 +59,25 @@ void inorder (Node* root) {
     inorder(root->right);
 }
 
+// Function to search for a value in the binary tree using DFS (Preorder)
+bool searchDFS (Node* root, int value) {
+    // Base case: If the tree is empty or we've reached a leaf node
+    if (root == nullptr) {
+        return false;
+    }
+
+    //If the node's data is equal to the value we are searching for 
+    if (root->data == value) {
+        return true;
+    }
+
+    // Recursively search in the left and right subtrees
+    bool left_res = searchDFS(root->left, value);
+    bool right_res = searchDFS(root->right, value);
+
+    return left_res || right_res;
+}
+
 int main () {
     Node* root = new Node(2);
     root->left = new Node(3);
@@ -75,5 +94,12 @@ int main () {
     cout << "Inorder traversal after insertion: ";
     inorder(root);
     cout << endl;
+
+    int value = 6;
+    if (searchDFS(root, value))
+        cout << value << " is found in the binary tree" << endl;
+    else
+        cout << value << " is not found in the binary tree" << endl;
+        
     return 0;
 }
